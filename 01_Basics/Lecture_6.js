e512aeda02482000f1dd3bc6d28a8af2f5a53987
Tilak Raj Panchal <139673387+tilakrajdev@users.noreply.github.com>
2024-09-08 12:39:24 +0000

    conversions

--- 01_Basics/Lecture_6.js
+++ 01_Basics/Lecture_6.js
@@ -29,5 +29,36 @@ console.log(typeof (str));
 */
 let num = 45
 let stringNumber = String(num)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ****************** Operations ************************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Tilak"
+
+let str3 = str1 + str2
+/*
+console.log(str3);
+
+// avoid these patterns in your code
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+*/
+
+console.log(true); // +true => 1 , true+ => error , +"" => false
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+console.log(num2)
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
