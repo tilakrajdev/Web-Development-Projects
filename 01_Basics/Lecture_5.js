3a5fdff561800827e3b9d3c15fca4083047499e1
Tilak Raj Panchal <139673387+tilakrajdev@users.noreply.github.com>
2024-09-08 12:38:44 +0000

    datatype summary

--- 01_Basics/Lecture_5.js
+++ 01_Basics/Lecture_5.js
@@ -13,5 +13,39 @@
    objects
 */
 
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+
+// **************** Datatypes ****************** //
+//Primitives
+// 7 types : String, Number, Boolean, null, undefined, Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// Non-Primitives (Reference type)
+// Array, Objects, Functions
+
+const heros = ["shaktiman","naagraj","daga"]; // Array
+
+let myObj = {
+   name: "hitesh",
+   age: 22,
+}
+
+const myFunction = function(){
+   console.log("Hello World");
+}
+
+console.log(typeof myObj);
+console.log(typeof (myFunction));
